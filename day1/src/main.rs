6c10f005a1250630bc901e2ca94d8636a502ac2d
jawood <jawood@expediagroup.com>
2023-12-01 16:24:00 +0000

    Day1 solved

---
+++ day1/src/main.rs
@@ -0,0 +1,68 @@
+use std::fs;
+use std::collections::HashMap;
+
+fn main() {
+    let file_path = "input.txt";
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    
+    let part1_string_numbers = contents.lines().map(|l| l.chars().filter(|c| c.is_numeric()).collect::<String>()).collect::<Vec<String>>();
+    let part1_first_char_iter = part1_string_numbers.iter().map(|s| s.chars().nth(0).unwrap().to_string());
+    let part1_last_char_iter = part1_string_numbers.iter().map(|s| s.chars().nth(s.len() - 1).unwrap().to_string());
+    let part1: i32 = std::iter::zip(part1_first_char_iter, part1_last_char_iter).map(|(l1, l2)| (l1 + &l2).parse::<i32>().unwrap()).sum();
+
+    let part1_one_liner : i32 =  contents.lines().map(|l| l.chars().filter(|c| c.is_numeric()).collect::<String>()).map(|s| (s.chars().nth(0).unwrap().to_string() + &s.chars().rev().nth(0).unwrap().to_string()).parse::<i32>().unwrap()).sum();
+    println!("Part 1: {}", part1_one_liner);
+
+    let mut parse_map = HashMap::<String, i32>::from([
+        (String::from("one"), 1),
+        (String::from("two"), 2),
+        (String::from("three"), 3),
+        (String::from("four"), 4),
+        (String::from("five"), 5),
+        (String::from("six"), 6),
+        (String::from("seven"), 7),
+        (String::from("eight"), 8),
+        (String::from("nine"), 9),
+    ]);
+
+    for k in 1..10 {
+        parse_map.insert(String::from(k.to_string()), k);
+    }
+
+    let part2: i32 = contents.lines().map(|l| part2_parse_line(l, &parse_map)).sum();
+
+    println!("Part 2: {}", part2);
+
+
+}
+
+fn part2_parse_line(line: &str, parse_map: &HashMap<String, i32>) -> i32 {
+
+    let mut left_amount = 0;
+    let mut right_amount = 0;
+    let mut left_ptr = 0;
+    let mut right_ptr = line.len();
+
+    while (left_amount == 0) || (right_amount == 0) {
+        
+        let left_substr = line[0..left_ptr+1].to_string();
+        let right_substr = line[right_ptr-1..].to_string();
+
+        for key in parse_map.keys() {
+            if left_substr.contains(key) && left_amount == 0 {
+                left_amount += parse_map.get(key).unwrap();
+            }
+            if right_substr.contains(key) && right_amount == 0 {
+                right_amount += parse_map.get(key).unwrap();
+            }
+        }
+
+        right_ptr -= (right_amount == 0) as usize;
+        left_ptr += (left_amount == 0) as usize;
+    }
+
+    (left_amount.to_string() + &right_amount.to_string()).parse::<i32>().unwrap()
+}
